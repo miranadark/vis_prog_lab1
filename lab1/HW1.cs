d65cd8ea1b47cc6a7c3330a738c72451fd5522a0
miranadark <57747648+miranadark@users.noreply.github.com>
2022-11-02 00:09:20 +0700

    Upload lab1

---
+++ lab1/HW1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visual_programming_project
+{
+	class HW1
+	{
+		public static long QueueTime(int[] customers, int n)
+		{
+			int min_cash_desks_index = 0, max_cash_desks_index = 0;
+			int[] cash_desks = new int[customers.Length];
+			for (int i = customers.GetLowerBound(0); i <= customers.GetUpperBound(0); i++)
+			{
+				cash_desks[i] = customers[i];
+			}
+			for (int i = n; i <= cash_desks.GetUpperBound(0); i++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					if (cash_desks[j] < cash_desks[min_cash_desks_index])
+						min_cash_desks_index = j;
+				}
+				cash_desks[min_cash_desks_index] += cash_desks[i];
+			}
+			for (int i = 0; i < n; i++)
+			{
+				if (cash_desks[i] > cash_desks[max_cash_desks_index])
+					max_cash_desks_index = i;
+			}
+			return cash_desks[max_cash_desks_index];
+		}
+	}
+}
---
+++ lab1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace visual_programming_project
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] customers_1 = { 5, 3, 4 };
+			int[] customers_2 = { 10, 2, 3, 3 };
+			int[] customers_3 = { 2, 3, 10 };
+			Console.WriteLine("Касса 1\tвермя\t" + HW1.QueueTime(customers_1, 1));
+			Console.WriteLine("Касса 2\tвермя\t" + HW1.QueueTime(customers_2, 2));
+			Console.WriteLine("Касса 3\tвермя\t" + HW1.QueueTime(customers_3, 2));
+		}
+	}
+}
